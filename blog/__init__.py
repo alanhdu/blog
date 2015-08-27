0a6b5290049f3242d3cd5129690805bf1554f050
Alan Du <alanhdu@gmail.com>
2015-08-26 20:15:24 -0400

    Add categories page

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,7 @@
 import datetime as dt
 
 from flask import Flask, render_template, abort, redirect, url_for
+import toolz
 
 app = Flask(__name__)
 
@@ -8,12 +9,16 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/categories")
+def categories():
+    groups = toolz.groupby(lambda x: x.metadata.category,
+                           app.config["BLOG"]["posts"])
+    return render_template("categories.html", groups=groups)
 
 def get_post(date: dt.date) -> "Optional[Post]":
     for p in app.config["BLOG"]["posts"]:
         if p.metadata.revdate == date:
             return p
-
 
 @app.route("/<int:year>/<int:month>/<int:day>/")
 def post(year, month, day):
