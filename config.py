8a7488b89598da16e9f203f5f027603194c816e3
Alan Du <alanhdu@gmail.com>
2019-10-20 18:39:04 -0400

    Start over with hugo

--- config.py
+++
@@ -1,6 +0,0 @@
-import urllib.parse
-import yaml
-
-with open("config.yml") as fin:
-    config = yaml.load(fin)
-config["base_path"] = urllib.parse.urlparse(config["base_url"]).path
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-source activate blog
-python run.py build
-scp -oHostKeyAlgorithms=+ssh-dss -r build/* ahd2125@cunix.cc.columbia.edu:~/public_html/
